e15ea1c36b8ded27aeb7c4106a3e185b41db0116
milymilek <milosz1596@wp.pl>
2025-02-10 19:41:08 +0100

    wip

--- retail_recommender_system/evaluation/metrics/map.py
+++ retail_recommender_system/evaluation/metrics/map.py
@@ -1,4 +1,20 @@
-import numpy as np
+import torch
+
+from retail_recommender_system.evaluation.evaluation import recommendation_relevance
 
 
-def map_k(): ...
+def ap_k(
+    recommendations: torch.Tensor, ground_truth: torch.Tensor, k: int, users_idx: torch.Tensor, n_users: int, n_items: int
+) -> torch.Tensor:
+    p_k = []
+    for _k in range(1, k + 1):
+        recommendations_k = recommendations[:, :_k]
+        rel, _, rel_mask = recommendation_relevance(recommendations_k, ground_truth, users_idx=users_idx, n_users=n_users, n_items=n_items)
+        p_k.append(torch.mean(rel[rel_mask], dim=1))
+    return torch.vstack(p_k).mean(dim=0).view(-1, 1)
+
+
+def map_k(
+    recommendations: torch.Tensor, ground_truth: torch.Tensor, k: int, users_idx: torch.Tensor, n_users: int, n_items: int
+) -> torch.Tensor:
+    return torch.mean(ap_k(recommendations, ground_truth, k, users_idx, n_users, n_items))
--- tests/evaluation/test_metrics.py
+++ tests/evaluation/test_metrics.py
@@ -1,6 +1,6 @@
 import torch
 
-from retail_recommender_system.evaluation.metrics import precision_k, recall_k
+from retail_recommender_system.evaluation.metrics import ap_k, map_k, precision_k, recall_k
 
 
 def test_precision_k(recommendations: torch.Tensor, ground_truth: torch.Tensor, users_idx: torch.Tensor, n_users: int, n_items: int):
@@ -17,3 +17,15 @@ def test_recall_k(recommendations: torch.Tensor, ground_truth: torch.Tensor, use
     for k, xrecall in zip(range(1, 5), expected_recalls):
         recall = recall_k(recommendations, ground_truth, k, users_idx, n_users, n_items)
         assert torch.isclose(recall, torch.tensor(xrecall), atol=1e-6)
+
+
+def test_map_k(recommendations: torch.Tensor, ground_truth: torch.Tensor, users_idx: torch.Tensor, n_users: int, n_items: int):
+    expected_mean_average_precisions = [0.0, 0.125, 7.0 / 36, 20.0 / 96]
+
+    # ap = ap_k(recommendations, ground_truth, 4, users_idx, n_users, n_items)
+    # print(ap)
+
+    for k, xmap in zip(range(1, 5), expected_mean_average_precisions):
+        map = map_k(recommendations, ground_truth, k, users_idx, n_users, n_items)
+        print(map)
+        assert torch.isclose(map, torch.tensor(xmap), atol=1e-6)
